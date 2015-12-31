ecc963741cfb877e99c75af41fd8b2181edd0342
Simon D. Levy <simon.d.levy@gmail.com>
2015-12-30 19:01:01 -0500

    Added copyright / license info

--- pyquadsim_server_extra.py
+++ pyquadsim_server_extra.py
@@ -1,3 +1,20 @@
+'''
+    pyquadsim_server_extra.py - Defines getAdditionalData function for server
+
+    Copyright (C) 2014 Bipeen Acharya, Fred Gisa, and Simon D. Levy
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as 
+    published by the Free Software Foundation, either version 3 of the 
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+'''
+
 def getAdditionalData(client, receiveFloats):
     return receiveFloats(client, 1)
 
